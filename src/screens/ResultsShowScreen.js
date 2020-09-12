680720a76791d1a1ccf7ef5eea03bd873214d4a0
santiagolopezsieiro <santiagolopezsieiro@gmail.com>
2020-09-12 11:21:48 -0300

    resultsShowScreen data and FlatList

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
@@ -7,17 +7,37 @@ const ResultsShowScreen = ({ navigation }) => {
   const id = navigation.getParam("id");
 
   const getResult = async (id) => {
-    const response = yelp.get(`${id}`);
-    setResults(response.data);
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
   };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
 
   return (
     <View>
-      <Text>results show</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+});
 
 export default ResultsShowScreen;
